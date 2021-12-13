e8d7a9770f0b20610c4ec26fa6b9fc30642ffddf
gtn1024 <gtn1024o@outlook.com>
2021-12-13 14:34:31 +0000

    Update at 2021-12-13T14:34:31.184903Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,10 +2,6 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap(point);
-    }
-
-    public static void swap(Point p) {
         int temp = p.x;
         p.x = p.y;
         p.y = temp;
