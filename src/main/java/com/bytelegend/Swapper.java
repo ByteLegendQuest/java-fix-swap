fea5c87ac30678d1301c0110e5660081df72a504
MeteorAndy <AndyLamwxl@Outlook.com>
2021-12-28 01:39:46 +0000

    Update at 2021-12-28T01:39:46.168407Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -5,9 +5,9 @@ public class Swapper {
         swap(point.x, point.y);
     }
 
-    public static void swap(int x, int y) {
-        int temp = x;
-        x = y;
-        y = temp;
+    public static void swap(Point p) {
+        int temp = p.x;
+        p.x = p.y;
+        p.y = temp;
     }
 }
