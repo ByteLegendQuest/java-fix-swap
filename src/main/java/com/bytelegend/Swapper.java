04bd306e32bca5d782cee21c905a4e531e2b3cbc
MeikleH <hxhmic@gmail.com>
2022-01-01 16:24:35 +0000

    Update at 2022-01-01T16:24:35.409590Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        int t = point.x;
+        int temp = point.x;
         point.x = point.y;
         point.y = t;
     }
