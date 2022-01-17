32f0c7feae19fee19fb50df5cf563978d8a58c88
gaozhidong <1011284295@qq.com>
2022-01-17 02:48:41 +0000

    Update at 2022-01-17T02:48:41.667357Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -6,8 +6,8 @@ public class Swapper {
     }
 
     public static void swap(int x, int y) {
-        int temp = x;
-        x = y;
-        y = temp;
+        int temp = point.x;
+        point.x = point.y;
+        point.y = temp;
     }
 }
