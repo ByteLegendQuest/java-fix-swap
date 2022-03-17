632fbfe357fcaec375f85a2448e9c70701dcbe51
lemon-altxx <2515782282@qq.com>
2022-03-17 07:05:57 +0000

    Update at 2022-03-17T07:05:57.240867Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,11 +2,11 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap(point);
+        swap(point,point.x,point.y);
     }
 
-    public static void swap(Point point) {
-        point.x = point.y;
-        point.y = point.x;
+    public static void swap(Point point,int x,int y) {
+        point.x = y;
+        point.y = x;
     }
 }
