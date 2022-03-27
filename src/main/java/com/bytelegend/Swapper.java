f6cb637bc35f795e803e6b9a9fc47129fbe01b36
greek-zzf <49605925+greek-zzf@users.noreply.github.com>
2022-03-27 10:33:35 +0000

    Update at 2022-03-27T10:33:35.858452Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,12 +2,10 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap(point.x, point.y);
+        point = swap(point.x, point.y);
     }
 
-    public static void swap(int x, int y) {
-        int temp = x;
-        x = y;
-        y = temp;
+    public static Point swap(int x, int y) {
+       return new Point(y, x);
     }
 }
