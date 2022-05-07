dfd73f5f45c241654a5ca5d49529a19047339d71
Longyang-Liu <2452791011@qq.com>
2022-05-07 05:45:20 +0000

    Update at 2022-05-07T05:45:20.874650Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,12 +2,12 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap(point.x, point.y);
+        swap(point);
     }
 
-    public static void swap(int x, int y) {
-        x = x + y;
-        y = x - y;
-        x = x - y;
+    public static void swap(point) {
+        point.x = point.x + point.y;
+        point.y = point.x - point.y;
+        point.x = point.x = point.y;
     }
 }
