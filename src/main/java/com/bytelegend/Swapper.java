90d75d819038ce545ff1989a0651cd64bcdd8505
zerofancy <yqmailsend@163.com>
2022-07-16 13:53:36 +0000

    Update at 2022-07-16T13:53:36.953417Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,12 +2,8 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap(point.x, point.y);
-    }
-
-    public static void swap(Point point) {
-        int temp = point.x;
+int temp = point.x;
         point.x = point.y;
-        point.y = temp;
-    }
+        point.y = temp;    }
+
 }
