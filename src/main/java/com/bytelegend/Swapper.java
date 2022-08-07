65771dabdee82a7525608e1162577740d11cee87
LearningThinking <iwannanewlife@outlook.com>
2022-08-07 09:01:30 +0000

    Update at 2022-08-07T09:01:30.099685Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,12 +2,8 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap2(point.x, point.y);
-    }
-
-    public static void swap2(int x, int y) {
-        int temp = x;
-        x = y;
-        y = temp;
+        int temp = point.x;
+        point x = point.y;
+        point y = temp;
     }
 }
