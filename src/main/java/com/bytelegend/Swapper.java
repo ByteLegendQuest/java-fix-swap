2138796fa6ddc3c46f205625af8a669aa6e4ecea
ZusmanFate <demonsLwg@foxmail.com>
2022-09-03 05:36:27 +0000

    Update at 2022-09-03T05:36:27.282340Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,15 +2,9 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        int []a=new int[2];
-        a[0]=point.x;
-        a[1]=point.y;
-        swap(a);
+        int temp = point.x;
+        point.x = point.y;
+        point.y = temp;
     }
 
-    public static void swap(int[]a) {
-        int temp = a[0];
-        a[0] = a[1];
-        a[1] = temp;
-    }
 }
