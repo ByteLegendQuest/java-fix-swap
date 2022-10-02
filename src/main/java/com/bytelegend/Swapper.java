33db7fac782debc1a198a06dd9d13934cce1c44e
MHPYGH <2950552644@qq.com>
2022-10-02 04:59:21 +0000

    Update at 2022-10-02T04:59:21.100386Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,11 +2,8 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap(point.x, point.y);
-    }
-
-    public static void swap(int x, int y) {
-       point.x = y;
-       point.y = x;
+       int x = point.y;
+       point.y = point.x;
+       point.x = x;
     }
 }
