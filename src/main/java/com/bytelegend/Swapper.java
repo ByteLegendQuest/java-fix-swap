3c1a326f50aa70e68976d5a9571b6ba497644a3d
ApoptosisToxin4869 <jiqianlang19660417@163.com>
2022-11-27 03:13:02 +0000

    Update at 2022-11-27T03:13:02.202804Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -1,13 +1,15 @@
 package com.bytelegend;
 
 public class Swapper {
+    Point point;
     public static void swap(Point point) {
+        this.point = point;
         swap(point.x, point.y);
     }
 
     public static void swap(int x, int y) {
         int temp = x;
-        x = y;
-        y = temp;
+        point.x = y;
+        point.y = temp;
     }
 }
