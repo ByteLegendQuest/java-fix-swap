ef77cafa62d70acacea215b3b1a146c0df05b8c0
darqlt <stungys@gmail.com>
2023-02-20 23:11:00 +0000

    Update at 2023-02-20T23:11:00.137806Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        int temp = x;
+        int temp = point.x;
         point.x = point.y;
         point.y = temp;
     }
