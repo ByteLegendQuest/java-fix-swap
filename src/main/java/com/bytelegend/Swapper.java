91c6daf7337318006ceb52d4ed7fb46e7ba4a599
ammbra <mihalceanuana@gmail.com>
2023-05-19 17:45:03 +0000

    Update at 2023-05-19T17:45:03.924362Z

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,8 +2,8 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        int temp = x;
-        x = y;
-        y = temp;    
+        int temp = point.x;
+        point.x = point.y;
+        point.y = temp;    
     }
 }
