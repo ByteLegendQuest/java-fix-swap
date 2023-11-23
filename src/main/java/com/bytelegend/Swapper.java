d9f06b69ec4de0e4c118adb851014fbd33609b34
VacancyIF <45335949+VacancyIF@users.noreply.github.com>
2023-11-23 14:08:23 +0800

    Update Swapper.java

--- src/main/java/com/bytelegend/Swapper.java
+++ src/main/java/com/bytelegend/Swapper.java
@@ -2,12 +2,15 @@ package com.bytelegend;
 
 public class Swapper {
     public static void swap(Point point) {
-        swap(point.x, point.y);
+//        swap(point.x, point.y);
+        int temp = point.x;
+        point.x = point.y;
+        point.y = temp;
     }
 
-    public static void swap(int x, int y) {
-        int temp = x;
-        x = y;
-        y = temp;
-    }
+//    public static void swap(int x, int y) {
+//        int temp = x;
+//        x = y;
+//        y = temp;
+//    }
 }
